7f22dd95955023c914b0e0d1c0901db51e4b8258
Ahmad Amin <ammyhunn@gmail.com>
2021-03-21 07:37:45 +0500

    binary search

--- Practice/binary_search.py
+++ Practice/binary_search.py
@@ -3,13 +3,13 @@ def search_target(arr, target):
     right = len(arr) - 1
     while left <= right:
         mid = int((left + right) / 2)
-        print(arr[mid])
         if target == arr[mid]:
             return mid
         elif target < arr[mid]:
             right = mid - 1
         else:
             left = mid + 1
+    return -1
 
 
 if __name__ == '__main__':
